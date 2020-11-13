1a2fafc4ba36ee7cbd3a2d60dbe07425645b97d1
ryanf10 <ryanfernaldy10@gmail.com>
2020-11-13 22:51:09 +0700

    minor fix seeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -14,14 +14,17 @@ class CategoriesTableSeeder extends Seeder
         //
         \App\Category::create([
             'category_name' => 'uncategorized',
+            'category_description' => 'temporary',
         ]);
 
         \App\Category::create([
             'category_name' => 'Pen',
+            'category_description' => 'untuk menulis dengan tinta',
         ]);
 
         \App\Category::create([
             'category_name' => 'Pencil',
+            'category_description' => 'untuk menulis',
         ]);
     }
 }
